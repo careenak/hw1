195afc938955ab910d88a97542af76340e7842cf
Karina <lyakhova_karina@list.ru>
2023-02-02 23:30:49 +0300

    первая задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+            Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+            Console.WriteLine("Введите первое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("Первое число " + x + " больше чем второе " + y);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + y + " больше чем первое " + x);
+            }
+            
